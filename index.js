301a51590fd4dc1305f755311d275caf046eaebc
LimbasiyaDev <limbasiyadev232004@gmail.com>
2026-05-28 16:12:07 +0530

    New Changes

--- index.js
+++ index.js
@@ -709,7 +709,7 @@ scheduleDailyReminderReset();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "*",
+  origin: process.env.FRONTEND_URL ? process.env.FRONTEND_URL.split(',') : true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
